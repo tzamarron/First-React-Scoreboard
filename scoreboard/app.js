317ea62aad6d01b5a23f6ed8ebf27908a81d4d79
Ted Zamarron <ted.zamarron@gmail.com>
2020-07-06 15:11:37 -0500

    using JSX

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,21 +1,19 @@
- //<h1 id="main-title" title="This is a title.">My First React Element!</h1>
-const title = React.createElement(
-    'h1', // ELement
-    { id: 'main-title', title: 'This is a title.'}, // Attributes
-    'My First React Element!' // Content
-);
+//  <h1 id="main-title" title="This is a title.">My First React Element!</h1>
+// const title = React.createElement(
+//     'h1', // ELement
+//     { id: 'main-title', title: 'This is a title.'}, // Attributes
+//     'My First React Element!' // Content
+// );
+// const title = 'My First React Element';
+const desc = 'I just learned how to create a React node and render it into the Dom';
+const myTitleID = 'main-title';
+const name =  'Ted';
 
-const desc = React.createElement(
-    'p',
-    {},
-    'I just learned how to create a React node and render it into the DOM'
-);
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
+const header = (
+    <header>
+        <h1 id={ myTitleID }>{ name }'s First React Element!</h1>
+        <p>{ desc }</p>
+    </header>
 );
 
 ReactDOM.render(
