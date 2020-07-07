36a16192dfaa3c48692ce29d584de8ab35081e49
Ted Zamarron <ted.zamarron@gmail.com>
2020-07-07 17:01:26 -0500

    added more to the scoreboard app using props and components

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -5,18 +5,72 @@
 //     'My First React Element!' // Content
 // );
 // const title = 'My First React Element';
-const desc = 'I just learned how to create a React node and render it into the Dom';
-const myTitleID = 'main-title';
-const name =  'Ted';
+const players = [
+    {
+        name: "Guil",
+        score: 50,
+        id: 1
+    },
+    {
+        name: "Treasure",
+        score: 85,
+        id: 2
+    },
+    {
+        name: "Ashley",
+        score: 95,
+        id: 3
+    },
+    {
+        name: "James",
+        score: 80,
+        id: 4
+    }
+]
 
-const header = (
+const Header = (props) => (
     <header>
-        <h1 id={ myTitleID }>{ name }'s First React Element!</h1>
-        <p>{ desc }</p>
+        <h1>{ props.title }</h1>
+        <span className="stats">Player : { props.totalPlayers }</span>
     </header>
 );
 
+const Player = (props) => (
+  <div className="player">
+      <span className="player-name">
+          { props.name }
+      </span>
+
+      <Counter score={ props.score }/>
+  </div>
+);
+
+const Counter = (props) => (
+    <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{ props.score }</span>
+        <button className="counter-action increment"> + </button>
+    </div>
+);
+
+const App = (props) => (
+    <div className="scoreboard">
+        <Header
+            title="Scoreboard"
+            totalPlayers={ props.initialPlayers.length }
+        />
+
+        {/* Players List */}
+        { props.initialPlayers.map( player =>
+            <Player
+                name={ player.name }
+                score={ player.score }
+                key={ player.id.toString() }
+            />
+        ) }
+    </div>
+);
 ReactDOM.render(
-    header, // What to show
+    <App initialPlayers={ players }/>, // What to show
     document.getElementById('root') // Where to show
 );
