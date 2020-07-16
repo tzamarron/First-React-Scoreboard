67ac92123599043c72fd997c38d810ba78ac6f2d
Ted Zamarron <ted.zamarron@gmail.com>
2020-07-16 09:01:04 -0500

    update state based on index functionality

--- scoreboard2/src/components/App.js
+++ scoreboard2/src/components/App.js
@@ -22,14 +22,20 @@ class App extends Component {
       },
       {
         name: "James",
-        score: 0, 
+        score: 0,
         id: 4
       }
     ]
   };
 
+  handleScoreChange = ( index, delta ) => {
+    this.setState(prevState => ({
+      score: prevState.players[index].score += delta
+    }));
+  }
+
   handleRemovePlayer = (id) => {
-    this.setState( prevState => {
+    this.setState(prevState => {
       return {
         players: prevState.players.filter(p => p.id !== id)
       };
@@ -39,19 +45,21 @@ class App extends Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header 
-          title="Scoreboard" 
-          totalPlayers={this.state.players.length} 
+        <Header
+          title="Scoreboard"
+          totalPlayers={this.state.players.length}
         />
-  
+
         {/* Players list */}
-        {this.state.players.map( player =>
-          <Player 
+        {this.state.players.map( (player, index) =>
+          <Player
             name={player.name}
             score={player.score}
             id={player.id}
-            key={player.id.toString()} 
-            removePlayer={this.handleRemovePlayer}           
+            key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange}
+            removePlayer={this.handleRemovePlayer}
           />
         )}
       </div>
