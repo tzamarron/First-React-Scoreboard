6a39c3b07fc2d306a6077b235997044e94287465
Ted Zamarron <ted.zamarron@gmail.com>
2020-07-15 21:22:29 -0500

    added remove items from state to players

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -13,13 +13,18 @@ const Header = (props) => (
     </header>
 );
 
-const Player = (props) => (
-    <div className="player">
-        <span className="player-name">{props.name}</span>
+const Player = (props) => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                <button className="remove-player" onClick={() => props.removePlayer( props.id ) }>x</button>
+                { props.name }
+                </span>
 
-        <Counter />
-    </div>
-);
+            <Counter />
+        </div>
+    );
+}
 
 class Counter extends React.Component {
     // constructor() {
@@ -100,20 +105,35 @@ class App extends React.Component {
         ]
     };
 
+    handleRemovePlayer = (id) => {
+        this.setState(prevState => {
+            return {
+                players: prevState.players.filter(p => p.id !== id)
+            }
+        })
+    }
+
     render() {
         return (
             <div className="scoreboard">
-                <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+                <Header title="Scoreboard"
+                    totalPlayers={this.state.players.length}
+                />
 
                 {/* Players List */}
-                {this.state.players.map((player) => (
-                    <Player name={player.name} key={player.id.toString()} />
-                ))}
+                {this.state.players.map(player =>
+                    <Player
+                        name={player.name}
+                        id={player.id}
+                        key={player.id.toString()}
+                        removePlayer={this.handleRemovePlayer}
+                    />
+                )}
             </div>
         )
     }
 }
-  
+
 ReactDOM.render(
     <App />, // What to show
     document.getElementById("root") // Where to show
