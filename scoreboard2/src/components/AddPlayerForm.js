5cd236098cf3584a4b19031c8393d54e9297d105
Ted Zamarron <ted.zamarron@gmail.com>
2020-07-20 16:07:31 -0500

    deconstructed props from addPlayer Form, Counter, Header, Player and Stats

--- scoreboard2/src/components/AddPlayerForm.js
+++ scoreboard2/src/components/AddPlayerForm.js
@@ -11,8 +11,9 @@ class AddPlayerForm extends Component {
     }
 
     handleSubmit = (e) => {
+        const {addPlayer} = this.props;
         e.preventDefault();
-        this.props.addPlayer(this.state.value);
+        addPlayer(this.state.value);
         this.setState({ value: '' })
     }
 
