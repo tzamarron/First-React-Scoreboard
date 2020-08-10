1e610225b73e2ca337f0f72fcfb2a58868420b5a
tzamarron <ted.zamarron@gmail.com>
2020-08-10 16:07:04 -0500

    added Navlink to Courses and used match props for nested links

--- react-router-4/1-Getting-Started-with-React-Router/3-Introducing-the-Project/course-directory/src/components/Courses.js
+++ react-router-4/1-Getting-Started-with-React-Router/3-Introducing-the-Project/course-directory/src/components/Courses.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { Route, NavLink } from "react-router-dom"
+import { Route, NavLink, Redirect } from "react-router-dom"
 
 import HTML from "./courses/HTML";
 import CSS from "./courses/CSS";
 import JavaScript from "./courses/JavaScript";
 
-const Courses = () => (
+const Courses = ({match}) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><NavLink to='/courses/html'>HTML</NavLink></li>
-        <li><NavLink to='/courses/css'>CSS</NavLink></li>
-        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
+        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
       </ul>
     </div>
     
     {/* Write routes here... */}
-    <Route path="/courses/html" component={HTML} />
-    <Route path="/courses/css" component={CSS} />
-    <Route path="/courses/javascript" component={JavaScript} />
+    <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/html`} /> } />
+    <Route path={`${match.path}/html`} component={HTML} />
+    <Route path={`${match.path}/css`} component={CSS} />
+    <Route path={`${match.path}/javascript`} component={JavaScript} />
     
   </div>
 );
