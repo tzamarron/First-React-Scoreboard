81e9d05a2a09a5a447d9147ad31033059d96e4fc
tzamarron <ted.zamarron@gmail.com>
2020-08-19 12:30:05 -0500

    installed and used axios

--- Fetch API/data-fetching-react/1-fetching-data-with-fetch-api/gif-search/src/App.js
+++ Fetch API/data-fetching-react/1-fetching-data-with-fetch-api/gif-search/src/App.js
@@ -1,38 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
 export default class App extends Component {
-  
+
   constructor() {
     super();
     this.state = {
       gifs: []
     };
   }
-  
+
   componentDidMount() {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({ gifs: responseData.data })
+    // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    //   .then(response => response.json())
+    //   .then(responseData => {
+    //     this.setState({ gifs: responseData.data })
+    //   })
+    //   .catch(error => {
+    //     console.log("Error fetching and parsing data", error)
+    //   });
+
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(response => {
+        this.setState({ 
+          gifs: response.data.data
+        })
       })
       .catch(error => {
-        console.log("Error fetching and parsing data", error)
+        console.log('Error fetching and parsing data', error);
       });
+
   }
 
-  render() { 
+  render() {
     console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
-          </div>   
-        </div>    
+            <SearchForm />
+          </div>
+        </div>
         <div className="main-content">
           <GifList />
         </div>
