b023762e9f17dc0eb98597855a799d0cb94fc50d
tzamarron <ted.zamarron@gmail.com>
2020-08-19 13:41:13 -0500

    added items from api to show on html

--- Fetch API/data-fetching-react/1-fetching-data-with-fetch-api/gif-search/src/Components/GifList.js
+++ Fetch API/data-fetching-react/1-fetching-data-with-fetch-api/gif-search/src/Components/GifList.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Gif from './Gif';
 
 const GifList = props => { 
+
+  const results = props.data;
+
+  let gifs = results.map(gif => 
+      <Gif url={gif.images.fixed_height.url} title={gif.title} key={gif.id} />
+    )
   
   return(
     <ul className="gif-list">
-      {/* <Gif /> */}
+      {gifs}
     </ul> 
   );
 }
